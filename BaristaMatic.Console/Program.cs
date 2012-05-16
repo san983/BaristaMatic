5d13a367a0af26ad67cfb1ed44644c7779b34a7d
Santiago Traversa <santiago.traversa@gmail.com>
2012-05-15 23:30:00 -0300

    Adding Test Project

---
+++ BaristaMatic.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BaristaMatic.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            System.Console.ReadKey();
+        }
+    }
+}
