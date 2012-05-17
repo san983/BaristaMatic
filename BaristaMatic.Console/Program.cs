fa2fb252992b20e0d18f828d799634db20d99c43
Santiago Traversa <santiago.traversa@gmail.com>
2012-05-17 18:38:05 -0300

    Renaming Test project 2

--- BaristaMatic.Console/Program.cs
+++ BaristaMatic.Console/Program.cs
@@ -4,19 +4,10 @@ namespace BaristaMatic.Console
 {
     public class Program
     {
-        CoffeeMachine _baristaMagic;
-
         public static void Main()
         {
-            new Program
-            {
-                _baristaMagic = new CoffeeMachine()
-            }.Run();
-        }
-
-        void Run()
-        {
-            _baristaMagic.Run();
+            var baristaMagicMachine = new CoffeeMachine();
+            baristaMagicMachine.Run();
         }
     }
 }
